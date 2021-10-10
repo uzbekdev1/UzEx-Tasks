99ed85fedb60b2faa6a0748af0c06125af907da2
Jasurbek Khasanboyev <81855769+jasurbuz@users.noreply.github.com>
2021-10-10 11:10:33 +0500

    added data project and indentity

---
+++ Jasurbek Xasanboyev/e-Shopping.Data/Contexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using e_Shopping.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace e_Shopping.Data.Contexts
+{
+    class AppDbContext : IdentityDbContext<ApiUser, Role, Guid>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+    }
+}
---
+++ Jasurbek Xasanboyev/e-Shopping.Data/Models/Events/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Shopping.Data.Models.Events
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public ICollection<ApiUser> Users { get; set; }
+            = new List<ApiUser>();
+    }
+}
